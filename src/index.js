0c3420a057880ef357c061538585a00d9656a6a1
Mckeydown <mckeydown@outlook.com>
2022-06-08 20:05:44 +0300

    path fix

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Puzzle from "./puzzle/App";
 import Moon from "./puzzle/moon/App"
-import Mars from './puzzle/mars/App'
 import NotFound from "./404/App"
 import Home from "./App";
-import T from './nblsoesagsb3sk6mx/App'
 
 
 export default function App() {
@@ -15,8 +13,6 @@ export default function App() {
           <Route index element={<Home />} />
           <Route path="puzzle" element={<Puzzle />} />
           <Route path="/puzzle/moon" element={<Moon />} />
-          <Route path="/puzzle/mars" element={<Mars />} />
-          <Route path="nblsoesagsb3sk6mx" element={<T/>}/>
           <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
