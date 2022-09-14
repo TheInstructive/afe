b9ffbe787eaa5f82a43dcd900f679fe4cc948d25
Mckeydown <mckeydown@outlook.com>
2022-09-15 01:12:29 +0300

    New Website

--- src/App.js
+++ src/App.js
@@ -1,137 +1,152 @@
+import React, { Component } from 'react';
 import './App.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter, faDiscord, faInstagram } from "@fortawesome/free-brands-svg-icons"
-import StarfieldAnimation from 'react-starfield-animation'
-import carix from './images/carix.png'
-import mon from './images/mon.png'
-import devalien from './images/devalien.png'
+import entry from "./images/main.png"
+import left_door from "./images/left_door.png"
+import right_door from "./images/right_door.png"
+import logo from "./images/logo.png"
+import menu from "./images/menu.png"
+import teams from './images/teams.png'
+import afelogo from './images/afe_logo.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="header">
-        <div className='logo'>AFE</div>
 
-        <div className='menu'>
-          <li><a href='/'>HOME</a></li>
-          <li><a href='#roadmap'>ROADMAP</a></li>
-          <li><a href='#team'>TEAM</a></li>
-        </div>
 
-        <div className='right-top'>
-          <a href='https://discord.gg/427XNcsVJV' target="_blank"><FontAwesomeIcon icon={faDiscord} /></a>
-          <a href='https://twitter.com/AFECronos' target="_blank"><FontAwesomeIcon icon={faTwitter} /></a>
-          {/*<a><FontAwesomeIcon icon={faInstagram} /></a>*/}
-        </div>
+class App extends Component
+ {
+  componentDidMount() {
+    let left_doors = document.getElementById("leftdoor")
+    let right_doors = document.getElementById("rightdoor")
+    let entry = document.getElementById("entry")
 
-      </header>
+      window.addEventListener('scroll', function(){
+        var value = window.scrollY;
+        console.log(value)
 
-      <div className='slider'>
-      <StarfieldAnimation
-        style={{
-          position: 'absolute',
-          width: '100%',
-          height: '800px'
-        }}
-      />
+        entry.style.transform = `scale(${1+value*0.008})`
 
-        <div className='slider-title'>
-        <a>ALIENS FROM EARTH</a>
-        <div class="spinner scroll-down"> <a class="animate"></a> </div>
-        </div>
+        left_doors.style.left = value*-4 + 'px'
+        right_doors.style.left = value*4+ 'px'
 
+        left_doors.style.transform = `scale(${1+value*0.01})`
+        right_doors.style.transform = `scale(${1+value*0.01})`
+
+        if (value > 250){
+          entry.style.display = "none"
+          left_doors.style.display = "none"
+          right_doors.style.display = "none"
+        }
+        else{
+          entry.style.display = "inline-block"
+          left_doors.style.display = "inline-block"
+          right_doors.style.display = "inline-block"
+        }
+
+      })
+  }
+
+  render() {
+    return (
+    <body>
+      <section>
+        <img src={entry} id="entry"></img>
+        <header id='header'>
+          <div></div>
+          <ul>
+            <li><a href='#' className='active-menu'>HOME</a></li>
+            <li><a href='#teams'>TEAM</a></li>
+            <li><a href='#'>ROADMAP</a></li>
+            <li><a href='#'>FAQ</a></li>
+          </ul>
+          <div></div>
+        </header>
+        <img src={afelogo} id="afelogo"></img> 
+        <img src={menu} id="menu_screen"></img>
+
+        <img src={right_door} id="rightdoor"></img>
+        <img src={left_door} id="leftdoor"></img>
+      </section>
+
+      <div className='in-site'>
+        <h2 className='menu-title' id='teams'>TEAMS</h2>
+        <img src={teams} id="team_screen"></img>
       </div>
 
-
-      <div className='title' id='roadmap'>ROADMAP</div>
-      <div className='border-container'><div className='border'></div></div>
-      <div className='roadmap-container'>
-        <div className='roadmap'>
-
-          {/*
-          <div className='left-step'>
-            <div className='l-step-dot'>
-            <div className='step'>STEP 1</div>
-            <div className='c-dot'></div>
-            </div>
-          <div className='step-ex'>Madde aciklaması buraya gelecek.</div>
-          </div>
-
-          <div className='left-step'>
-            <div className='l-step-dot'>
-            <div className='step'>STEP 2</div>
-            <div className='n-dot'></div>
-            </div>
-          <div className='step-ex'>Madde aciklaması buraya gelecek.</div>
-          </div>
-
-          <div className='left-step'>
-            <div className='l-step-dot'>
-            <div className='step'>STEP 3</div>
-            <div className='n-dot'></div>
-            </div>
-          <div className='step-ex'>Madde aciklaması buraya gelecek.</div>
-          </div>
-        
-        */
-      }
-              </div> 
+      <div className='sec'>
+          <h2>About US</h2>
+          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br></br>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br></br>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br></br>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br></br></p>
       </div>
-
-{/* 
-      <div className='story' id='story'>
-        <div className='title'>STORY</div>
-        <div className='border-container'><div className='border'></div></div>
-
-        <div className='story-container'>
-          <div className='story-left'>
-            <img src='https://picsum.photos/300/300'></img>
-          </div>
-
-          <div className='story-right'>
-            <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-          </div>
-
-        </div>
-
-      </div>
- */}
-
-      <div className='team' id='team'>
-      <div className='title'>TEAM</div>
-      <div className='border-container'><div className='border'></div></div>
-
-        <div className='team-members'>
-        <div className='member'>
-          <div style={{fontWeight:'800'}} className='member-name'>DEVALIEN</div>
-          <div className='avatar'><img width={'100%'} src={devalien}></img></div>
-          <div style={{fontSize:'12pt'}}  className='member-name'>BACKEND DEVELOPER</div>
-        </div>
-
-        <div className='member'>
-        <div style={{fontWeight:'800'}} className='member-name'>MON</div>
-          <div className='avatar'><img width={'100%'} src={mon}></img></div>
-          <div style={{fontSize:'12pt'}}  className='member-name'>ARTIST</div>
-        </div>
-
-        <div className='member'>
-        <div style={{fontWeight:'800'}} className='member-name'>CARIX</div>
-          <div className='avatar'><img width={'100%'} src={carix}></img></div>
-          <div style={{fontSize:'12pt'}} className='member-name'>FRONTEND DEVELOPER</div>
-        </div>
-
-        </div>
-      </div>
-
-      <div className='footer'>
-
-      </div>
-
-
-    </div>
-  );
+    </body>
+     )
+  }
 }
-
 export default App;
+
+
+{/**
+
+    <div className='doors' style={{width:width}}>
+      <div className='left-door'>
+      <img id='door_lefts' height={width/3.30} src={left_door} style={{marginTop:width/doorsWidth, marginLeft:-width/6.5}}></img>
+      </div>
+      <div className='right-door'>
+      <img id='door_rights' height={width/3.30} src={right_door} style={{marginTop:width/doorsWidth, marginRight:-width/6.5}}></img>
+      </div>
+    </div>
+    
+    <div className='main'>
+      <img width={width} src={entry}></img>
+    </div>
+
+          <div className='entry'>
+      <img width={width} src={entry}></img>
+      </div>
+
+
+      <div style={{height:height, width:width}} className='doors'>
+
+        <div className='left-door'>
+          <img style={{transform:`translate(${width/55}vh, 1.8vh)`}} height={width/3.33} id='ldoor' src={left_door}></img>
+        </div>
+
+        <div className='right-door'>
+          <img style={{transform:`translate(${-width/56.8}vh, 1.8vh)`}} height={width/3.33} id='rdoor' src={right_door}></img>
+        </div>
+      </div>
+
+      <div className='section'>
+        <h2>TEST</h2>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br></br>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
+        It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.<br></br>
+    
+        </p>
+
+      </div>
+
+*/}
---
+++ src/App1.js
@@ -0,0 +1,137 @@
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter, faDiscord, faInstagram } from "@fortawesome/free-brands-svg-icons"
+import StarfieldAnimation from 'react-starfield-animation'
+import carix from './images/carix.png'
+import mon from './images/mon.png'
+import devalien from './images/devalien.png'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <div className='logo'>AFE</div>
+
+        <div className='menu'>
+          <li><a href='/'>HOME</a></li>
+          <li><a href='#roadmap'>ROADMAP</a></li>
+          <li><a href='#team'>TEAM</a></li>
+        </div>
+
+        <div className='right-top'>
+          <a href='https://discord.gg/427XNcsVJV' target="_blank"><FontAwesomeIcon icon={faDiscord} /></a>
+          <a href='https://twitter.com/AFECronos' target="_blank"><FontAwesomeIcon icon={faTwitter} /></a>
+          {/*<a><FontAwesomeIcon icon={faInstagram} /></a>*/}
+        </div>
+
+      </header>
+
+      <div className='slider'>
+      <StarfieldAnimation
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '800px'
+        }}
+      />
+
+        <div className='slider-title'>
+        <a>ALIENS FROM EARTH</a>
+        <div class="spinner scroll-down"> <a class="animate"></a> </div>
+        </div>
+
+      </div>
+
+
+      <div className='title' id='roadmap'>ROADMAP</div>
+      <div className='border-container'><div className='border'></div></div>
+      <div className='roadmap-container'>
+        <div className='roadmap'>
+
+          {/*
+          <div className='left-step'>
+            <div className='l-step-dot'>
+            <div className='step'>STEP 1</div>
+            <div className='c-dot'></div>
+            </div>
+          <div className='step-ex'>Madde aciklaması buraya gelecek.</div>
+          </div>
+
+          <div className='left-step'>
+            <div className='l-step-dot'>
+            <div className='step'>STEP 2</div>
+            <div className='n-dot'></div>
+            </div>
+          <div className='step-ex'>Madde aciklaması buraya gelecek.</div>
+          </div>
+
+          <div className='left-step'>
+            <div className='l-step-dot'>
+            <div className='step'>STEP 3</div>
+            <div className='n-dot'></div>
+            </div>
+          <div className='step-ex'>Madde aciklaması buraya gelecek.</div>
+          </div>
+        
+        */
+      }
+              </div> 
+      </div>
+
+{/* 
+      <div className='story' id='story'>
+        <div className='title'>STORY</div>
+        <div className='border-container'><div className='border'></div></div>
+
+        <div className='story-container'>
+          <div className='story-left'>
+            <img src='https://picsum.photos/300/300'></img>
+          </div>
+
+          <div className='story-right'>
+            <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+            </p>
+          </div>
+
+        </div>
+
+      </div>
+ */}
+
+      <div className='team' id='team'>
+      <div className='title'>TEAM</div>
+      <div className='border-container'><div className='border'></div></div>
+
+        <div className='team-members'>
+        <div className='member'>
+          <div style={{fontWeight:'800'}} className='member-name'>DEVALIEN</div>
+          <div className='avatar'><img width={'100%'} src={devalien}></img></div>
+          <div style={{fontSize:'12pt'}}  className='member-name'>BACKEND DEVELOPER</div>
+        </div>
+
+        <div className='member'>
+        <div style={{fontWeight:'800'}} className='member-name'>MON</div>
+          <div className='avatar'><img width={'100%'} src={mon}></img></div>
+          <div style={{fontSize:'12pt'}}  className='member-name'>ARTIST</div>
+        </div>
+
+        <div className='member'>
+        <div style={{fontWeight:'800'}} className='member-name'>CARIX</div>
+          <div className='avatar'><img width={'100%'} src={carix}></img></div>
+          <div style={{fontSize:'12pt'}} className='member-name'>FRONTEND DEVELOPER</div>
+        </div>
+
+        </div>
+      </div>
+
+      <div className='footer'>
+
+      </div>
+
+
+    </div>
+  );
+}
+
+export default App;
